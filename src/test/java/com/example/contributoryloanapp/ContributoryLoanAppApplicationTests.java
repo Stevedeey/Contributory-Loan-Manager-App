e1689976c349b1a8cb54d48e9bf4fc51c83b9660
stevedeey <stvolutosin69@gmail.com>
2021-07-28 21:30:05 +0100

    CI_CD related test fixed

--- src/test/java/com/example/contributoryloanapp/ContributoryLoanAppApplicationTests.java
+++ src/test/java/com/example/contributoryloanapp/ContributoryLoanAppApplicationTests.java
@@ -2,11 +2,8 @@ package com.example.contributoryloanapp;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 
 @ExtendWith(MockitoExtension.class)
 class ContributoryLoanAppApplicationTests {
